a70f29a834d83892136da3417e2d6340e25bfd14
AeredrenInsa <romain.lebrun-thauront@insa-lyon.fr>
2019-06-05 16:43:44 +0200

    try sthing

--- consul-cluster-manager/src/main/java/io/vertx/spi/cluster/consul/impl/ConsulCompressAsyncMap.java
+++ consul-cluster-manager/src/main/java/io/vertx/spi/cluster/consul/impl/ConsulCompressAsyncMap.java
@@ -1,8 +1,25 @@
 package io.vertx.spi.cluster.consul.impl;
 
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.core.shareddata.AsyncMap;
+import io.vertx.core.shareddata.LocalMap;
+import io.vertx.core.shareddata.Lock;
+import io.vertx.core.spi.cluster.ClusterManager;
+import io.vertx.ext.consul.KeyValueOptions;
+
+import java.util.*;
 import java.util.zip.Deflater;
 import java.util.zip.Inflater;
-import java.util.*;
+
+import static io.vertx.core.Future.failedFuture;
+import static io.vertx.core.Future.succeededFuture;
+import static io.vertx.spi.cluster.consul.impl.ConversationUtils.asFutureString;
+import static io.vertx.spi.cluster.consul.impl.ConversationUtils.asTtlConsulEntry;
 
 public class ConsulCompressAsyncMap extends ConsulAsyncMap {
 
@@ -12,7 +29,7 @@ public class ConsulCompressAsyncMap extends ConsulAsyncMap {
 
    @Override
    private Future<Boolean> putValue(K k, V v, KeyValueOptions keyValueOptions, Optional<Long> ttl) {
-      byte[] input = v.getBytes("UTF-8");
+
       Long ttlValue = ttl.map(aLong -> ttl.get()).orElse(null);
       return asFutureString(k, v, appContext.getNodeId(), ttlValue)
         .compose(value -> putPlainValue(keyPath(k), value, keyValueOptions))
@@ -20,6 +37,24 @@ public class ConsulCompressAsyncMap extends ConsulAsyncMap {
           ttlMonitor.apply(keyPath(k), ttl)
             .compose(aVoid -> succeededFuture(result)));
     }
+
+    private String compressStr (String value) {
+      try {
+         byte[] input = value.getBytes("UTF-8");
+
+         // Compress the bytes
+         byte[] output = new byte[input.length];
+         Deflater compresser = new Deflater();
+         compresser.setInput(input);
+         compresser.finish();
+         int compressedDataLength = compresser.deflate(output);
+         compresser.end();
+      }catch(java.io.UnsupportedEncodingException ex) {
+         System.exit(0);
+      } catch (java.util.zip.DataFormatException ex) {
+         System.exit(0);
+      }
+   }
 }
 
 /*
