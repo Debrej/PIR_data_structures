d19c5f1282a4c046093d2aa33f517be0df5601a4
robfil62 <robinfillion@gmail.com>
2019-06-14 12:48:41 +0200

    Issue lock

--- hazelcast/SharedDataReader/src/main/java/com/kodcu/main/Starter.java
+++ hazelcast/SharedDataReader/src/main/java/com/kodcu/main/Starter.java
@@ -8,6 +8,8 @@ import com.kodcu.sdr.verticle.ReaderFileInfini;
 
 
 import io.vertx.core.Vertx;
+import io.vertx.core.shareddata.*;
+
 import io.vertx.core.VertxOptions;
 import io.vertx.core.spi.cluster.ClusterManager;
 import io.vertx.spi.cluster.hazelcast.HazelcastClusterManager;
@@ -53,13 +55,15 @@ public class Starter {
             Vertx.clusteredVertx(options, cluster -> {
                 if (cluster.succeeded()) {
                   for(int i=0;i<nmbSearch;i++){
+                    // SharedData.getLock("myLock", lock -> {
 
-                    cluster.result().deployVerticle(new ReaderFile(keys,writer), res -> {
-                        if (res.succeeded()) {
-                            log.info("Deployment id is: {} ", res.result());
-                        } else {
-                            log.error("Deployment failed!", res.cause());
-                        }
+                      cluster.result().deployVerticle(new ReaderFile(keys,writer), res -> {
+                          if (res.succeeded()) {
+                              log.info("Deployment id is: {} ", res.result());
+                          } else {
+                              log.error("Deployment failed!", res.cause());
+                          }
+                      });
                     });
                   }
                 } else {
@@ -73,33 +77,7 @@ public class Starter {
         catch (Exception e) {
             e.printStackTrace();
         }
-      //   for(int i=0;i<nmbReader-1;i++){
-      //     try {
-      //         String membersStr = new String(Files.readAllBytes(Paths.get(args[0])));
-      //         String[] members = membersStr.split("\n");
-      //         String keysStr = new String(Files.readAllBytes(Paths.get(args[2])));
-      //         String[] keys = keysStr.split("\n");
-      //
-      //         final ClusterManager mgr = new HazelcastClusterManager(ClusterConfiguratorHelper.getHazelcastConfigurationSetUp(members, args[1]));
-      //         final VertxOptions options = new VertxOptions().setClusterManager(mgr);
-      //         Vertx.clusteredVertx(options, cluster -> {
-      //             if (cluster.succeeded()) {
-      //                 cluster.result().deployVerticle(new ReaderFileInfini(keys), res -> {
-      //                     if (res.succeeded()) {
-      //                         log.info("Deployment id is: {} ", res.result());
-      //                     } else {
-      //                         log.error("Deployment failed!", res.cause());
-      //                     }
-      //                 });
-      //             } else {
-      //                 log.error("Cluster up failed!", cluster.cause());
-      //             }
-      //         });
-      //     }
-      //     catch (java.io.IOException e) {
-      //         e.printStackTrace();
-      //     }
-      // }
+
     }
 
 
